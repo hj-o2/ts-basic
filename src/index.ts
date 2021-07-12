26eeae7c8ae62673d0e1fbeb639689538666f031
北條 祐輝 <hojo@itodenko2.local>
2021-07-12 11:08:02 +0900

    ts10

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+// #03.「環境構築」と「ESLint&Prettier」のサンプルコード
 // import World from './world'
 
 // const root = document.getElementById('root')
 // const world = new World('Hello World')
 // world.sayHello(root)
 
-// 03.基本の型定義
+// #04.基本の型定義
 // import { notExistSample, primitiveSample, anySample, unknownSample } from './basic'
 
 // notExistSample()
@@ -12,7 +13,7 @@
 // anySample()
 // unknownSample()
 
-// 04.関数の型定義
+// #05.関数の型定義
 // import {logMessage} from './function/basic'
 // import {isUserSignedIn, isUserSignedIn2, sumProductsPrice} from './function/parameters'
 
@@ -23,20 +24,26 @@
 // const sum = sumProductsPrice(100, 200, 300, 400, 500)
 // console.log('Function parameters sample 5:', sum);
 
-// 05.オブジェクトの型定義
-import objectSample from './object/object'
-import typeAliasSample from './object/alias'
-objectSample()
-typeAliasSample()
+// #06.オブジェクトの型定義
+// import objectSample from './object/object'
+// import typeAliasSample from './object/alias'
+// objectSample()
+// typeAliasSample()
 
-// 06.配列とタプルの型定義
+// #07.配列とタプルの型定義
 // import arraySample from './array/array'
 // import tupleSample from './array/tuple'
 // arraySample()
 // tupleSample()
 
-// 07.ジェネリック型とポリモーフィズム
+// #08.ジェネリック型とポリモーフィズム
 // import genericsBasicSample from './generics/basic'
 // import genericsAdvancedSample from './generics/advanced'
 // genericsBasicSample()
 // genericsAdvancedSample()
+
+// #09.TypeScriptで学ぶオブジェクト指向開発
+
+// #10.インターフェイスとtypeエイリアス
+import interfaceSample from './interface/interface'
+interfaceSample()
